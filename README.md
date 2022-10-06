71cb1a2346c75ae1c671167e224900eb6aa3c22a
Guilherme Henrique Couto de Jesus <guicoutoo@outlook.com>
2022-10-06 16:57:17 -0300

    [x] Só forka

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# looker-react-athena
+# Embed Looker Into React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+Look at `src/components/Dashboard.js` for an example of the SSO embed.
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Header";
+
+import "./App.css";
+
+const App = (props) => {
+  return (
+    <div className="App">
+      <Header />
+      <Dashboard />
+    </div>
+  );
+};
+
+export default App;
